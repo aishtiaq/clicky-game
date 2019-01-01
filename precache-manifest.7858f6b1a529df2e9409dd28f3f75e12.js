4f278f25775e4e669eb9444beb8062127603c40e
Abeer Ishtiaq <mabeer@hotmail.com>
2018-12-31 21:03:10 -0500

    Updates

---
+++ precache-manifest.7858f6b1a529df2e9409dd28f3f75e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "945131ae85e86fe041ec",
+    "url": "/clicky-game/static/js/main.945131ae.chunk.js"
+  },
+  {
+    "revision": "d9faa6b73786850bf0ec",
+    "url": "/clicky-game/static/js/1.d9faa6b7.chunk.js"
+  },
+  {
+    "revision": "945131ae85e86fe041ec",
+    "url": "/clicky-game/static/css/main.846ec393.chunk.css"
+  },
+  {
+    "revision": "e49808a5fdd9036f015ec7a6e34046bd",
+    "url": "/clicky-game/index.html"
+  }
+];
